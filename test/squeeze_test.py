eb97a547fb17f4674ec04c5e4ed70c852848910c
Ryan Kadwell <ryan@riaka.ca>
2014-06-13 08:57:13 -0600

    Remove old tests

--- test/squeeze_test.py
+++ test/squeeze_test.py
@@ -8,14 +8,4 @@ from gitsqueeze import *
 
 class TestSqueeze(unittest.TestCase):
 
-   def test_parse_diff_line(self):
-      s = Squeeze()
-      self.assertEqual(
-         s.parse_diff_line("A\ttestfile"),
-         ("A", ["testfile"])
-      )
-
-      self.assertEqual(
-         s.parse_diff_line("C100\ttestfile\ttest"),
-         ("C100", ["testfile", "test"])
-      )
+   pass
