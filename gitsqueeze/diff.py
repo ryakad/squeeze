7e707681cbe4a6c1db1d12415eef183b8f2aa96e
Ryan Kadwell <ryan@riaka.ca>
2014-06-13 08:50:47 -0600

    Pull git diffing code out into its own object

---
+++ gitsqueeze/diff.py
@@ -0,0 +1,104 @@
+#! /usr/bin/env python
+#
+# GitSqueeze
+# Copyright (c) Ryan Kadwell <ryan@riaka.ca>
+#
+# Contains classes to generate diff data from repo
+#
+# Author: Ryan Kadwell <ryan@riaka.ca>
+#
+
+import squeeze
+from util import Command
+
+
+class GitDiff(object):
+   """Find diff data for a git repository"""
+
+   def __init__(self, path, rename_similarity=100, copy_similarity=100, branch="master"):
+      self.base_path = path
+      self.rename_similarity = rename_similarity
+      self.copy_similarity = copy_similarity
+      self.branch = branch
+
+   def diff(self, a, b):
+      """Returns diff data representing delta required to from commit a to b
+
+         diff will return an array of tuples in the format (DELTA, [files])
+         where delta is one of:
+
+         squeeze.FILE_ADDED
+         squeeze.FILE_DELETED
+         squeeze.FILE_MODIFIED
+         squeeze.FILE_COPIED
+         squeeze.FILE_RENAMED
+      """
+      changes = []
+
+      if a == b:
+         # Nothing to do.
+         pass
+
+      elif not a:
+         # Everything in repo is new since we dont have a starting point
+         returncode, stdout, stderr = Command.run(
+            ['git', 'ls-tree', '-r', self.branch, '--name-only'],
+            cwd=self.base_path
+         )
+
+         if not returncode == 0:
+            self.exit('Unable to list the git tree to file files in project')
+
+         for line in stdout:
+            changes.append((squeeze.FILE_ADDED, [line]))
+      else:
+         diff = "{0}..{1}".format(self.last_run, latest_hash)
+
+         returncode, stdout, stderr = Command.run(
+            ['git', 'diff', '--name-status', '-C', diff]
+         )
+
+         if not returncode == 0:
+            raise Exception("Unable to find changed files")
+
+         for line in stdout:
+            changes = changes + self._parse_diff_line(line)
+
+      return changes
+
+   def _parse_diff_line(self, line):
+      """Parse lines from git diff to the desired format
+
+         Converts the lines returned from git diff into a tule contining the
+         change type as the first parameter and an array of effected files as
+         the second parameter.
+      """
+      # TODO This does not respect tabs in filenames. Tho rare and discouraged
+      # it is allowed and should be handled properly.
+      parts = re.split(r'\t+', line.strip())
+
+      changetype = parts[0]
+      files = parts[1:]
+
+      if changetype == "A":
+         return [(squeeze.FILE_ADDED, files)]
+
+      elif changetype == "M":
+         return [(squeeze.FILE_MODIFIED, files)]
+
+      elif changetype == "D":
+         return [(squeeze.FILE_MODIFIED, files)]
+
+      elif re.match(r'^R[0-9]+$', changetype):
+         similarity = int(changetype.lstrip("R"))
+         if similarity >= self.rename_similarity:
+            return [(squeeze.FILE_RENAMED, files)]
+         else:
+            return [(squeeze.FILE_DELETED, files[0]), (squeeze.FILE_ADDED, files[1])]
+
+      elif re.match(r'^C[0-9]+$', changetype):
+         similarity = int(changetype.lstrip("C"))
+         if similarity >= self.copy_similarity:
+            return [(squeeze.FILE_COPIED, files)]
+         else:
+            return [(squeeze.FILE_ADDED, files[1])]
--- gitsqueeze/squeeze.py
+++ gitsqueeze/squeeze.py
@@ -13,7 +13,7 @@ import re
 import logbook
 
 from util import Command, createPIDLockFile, removePIDLockFile
-
+from diff import GitDiff
 
 # Flags for change types
 FILE_ADDED = 1
@@ -119,17 +119,6 @@ class Squeeze(object):
 
    def parse(self):
       try:
-         # Make sure that we are on the proper branch to process.
-         returncode, stdout, stderr = Command.run(['git', 'checkout', self.branch])
-         if not returncode == 0:
-            self.exit('Unable to checkout the master branch')
-
-         # If we are taking changes to process from a remote check for them now.
-         if self.remote:
-            returncode, stdout, stderr = Command.run(['git', 'pull', self.remote, self.branch])
-            if not returncode == 0:
-               self.exit('Unable to fetch remote changes', 1)
-
          # Find what commit we are processing up to.
          returncode, stdout, stder = Command.run(['git', 'rev-list', '--all'])
          if not returncode == 0:
@@ -142,45 +131,20 @@ class Squeeze(object):
 
          latest_hash = revlist[0]
 
-         if latest_hash == self.last_run:
-            self.logger.notice('Nothing to do.')
-            self._cleanup()
-            sys.exit(0)
-         elif not self.last_run:
-            # need to handle all files under revision as additions
-            returncode, stdout, stderr = Command.run(
-               ['git', 'ls-tree', '-r', self.branch, '--name-only'],
-               cwd=self.project_base_dir
-            )
-
-            if not returncode == 0:
-               self.exit('Unable to list the git tree to file files in project')
-
-            for line in stdout:
-               self.logger.notice('ADDED {0}'.format(line));
-               for function in self.get_handlers_for(FILE_ADDED):
-                  function(FILE_ADDED, line)
-
-         elif self.last_run not in revlist:
+         # make sure that we can even find the last commit in the tree.
+         if self.last_run and self.last_run != latest_hash and self.last_run not in revlist:
             msg = 'Commit "{0}" not in rev-list'.format(self.last_run)
             self.logger.error(msg)
             self.exit(msg)
 
-         else:
-            # process files changed between commits
-            diff = "{0}..{1}".format(self.last_run, latest_hash)
+         # TODO: OFFLOAD TO GIT TO GET DIFF LIST
+         self.logger.notice("Querying changes from {0} to {1}.".format(self.last_run, latest_hash))
+         gd = GitDiff(self.project_base_dir)
+         changes = gd.diff(self.last_run, latest_hash)
 
-            self.logger.notice("Processing changes from {0} to {1}.".format(self.last_run, latest_hash))
-
-            returncode, stdout, stderr = Command.run(
-               ['git', 'diff', '--name-status', '-C', diff]
-            )
-
-            if not returncode == 0:
-               self.exit('Unable to find changed files')
-
-            for line in stdout:
-               self._process_diff(line)
+         for changetype, files in changes:
+            for function in self.get_handlers_for(changetype):
+               function(changetype, *files)
 
          self.last_run = latest_hash
 
@@ -195,81 +159,12 @@ class Squeeze(object):
          self.logger.error("Unable to remove lockfile")
          self.exit("Unable to remove lockfile. IF you are sure no other process is running you may remove the file {0} manually and try again".format(self.lockfile))
 
-   def _process_diff(self, line):
-      self.logger.debug('Called _process_diff for line: {0}'.format(line))
-      changetype, files = self.parse_diff_line(line)
-
-      if changetype == "A":
-         self.logger.notice('ADDED {0}'.format(files[0]))
-         for function in self.get_handlers_for(FILE_ADDED):
-            function(FILE_ADDED, files[0])
-
-      elif changetype == "M":
-         self.logger.notice('MODIFIED {0}'.format(files[0]))
-         for function in self.get_handlers_for(FILE_MODIFIED):
-            function(FILE_MODIFIED, files[0])
-
-      elif changetype == "D":
-         self.logger.notice('DELETED {0}'.format(files[0]))
-         for function in self.get_handlers_for(FILE_DELETED):
-            function(FILE_DELETED, files[0])
-
-      elif re.match(r'^R[0-9]+$', changetype):
-         # We will check the similarity of the renamed file and if it is
-         # less than deplou.rename-similarity we will use count it as an
-         # addition and removal. Handling the removals first.
-         similarity = int(changetype.lstrip("R"))
-         if not similarity == self.get_config('squeeze.rename-similarity', 100):
-            self.logger.notice('RENAMED {0} -> {1}'.format(files[0], files[1]))
-            for function in self.get_handlers_for(FILE_RENAMED):
-               function(FILE_RENAMED, files[0], files[1])
-         else:
-            self.logger.notice('DELETED {0}'.format(files[0]))
-            self.logger.notice('ADDED {0}'.format(files[0]))
-            for function in self.get_handlers_for(FILE_DELETED):
-               function(FILE_DELETED, files[0])
-            for function in self.get_handlers_for(FILE_ADDED):
-               function(FILE_ADDED, files[0])
-
-      elif re.match(r'^C[0-9]+$', changetype):
-         # We handle this in a similar method to the rename. If the
-         # similarity is less than squeeze.copy-similarity we will count
-         # it as an addition.
-         similarity = int(changetype.lstrip("C"))
-         if similarity == self.get_config('squeeze.copy-similarity', 100):
-            self.logger.notice('COPIED {0} -> {1}'.format(files[0], files[1]))
-            for function in self.get_handlers_for(FILE_COPIED):
-               function(FILE_COPIED, files[0], files[1])
-         else:
-            self.logger.notice('ADDED {0}'.format(files[0]))
-            for function in self.get_handlers_for(FILE_ADDED):
-               function(FILE_ADDED, files[1])
-
-      else:
-         self.exit("Unhandled change type " + changetype)
-
    def get_handlers_for(self, delta):
       funcs = []
       for index in [x for x in self.handlers if x & delta]:
          funcs = funcs + self.handlers[index]
 
       return funcs
-
-   def parse_diff_line(self, line):
-      """Parse lines from git diff to the desired format
-
-         Converts the lines returned from git diff into a tule contining the
-         change type as the first parameter and an array of effected files as
-         the second parameter.
-      """
-      # TODO This does not respect tabs in filenames. Tho rare and discouraged
-      # it is allowed and should be handled properly.
-      parts = re.split(r'\t+', line.strip())
-
-      changetype = parts[0]
-      files = parts[1:]
-
-      return changetype, files
 
    def exit(self, message, exitcode=1):
       """Writes a message to stderror"""
