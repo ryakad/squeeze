914c392ccc66ca914b3e7aea7a95b6c6a48f656d
Ryan Kadwell <ryan@riaka.ca>
2014-06-12 18:30:39 -0600

    Remove unused pidfile

--- gitsqueeze/pidfile.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-#
-# git-squeeze
-# Copyright (c) Ryan Kadwell <ryan@riaka.ca>
-#
-# Script to process file changes on a git repository as commits are made.
-#
-# Author: Ryan Kadwell <ryan@riaka.ca>
